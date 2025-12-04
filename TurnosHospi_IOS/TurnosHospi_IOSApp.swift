f33657ae20138e31878b22435b200c7dffa53518
Sote4 <andresmend12@gmail.com>
2025-12-04 15:31:42 +0100

    reinicio

--- TurnosHospi_IOS/TurnosHospi_IOSApp.swift
+++ TurnosHospi_IOS/TurnosHospi_IOSApp.swift
@@ -1,31 +1,17 @@
-import SwiftUI
-import FirebaseCore
+//
+//  TurnosHospi_IOSApp.swift
+//  TurnosHospi_IOS
+//
+//  Created by Andrés Mendoza Domínguez on 4/12/25.
+//
 
-// 1. Configurador de AppDelegate para inicializar Firebase
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
-}
+import SwiftUI
 
 @main
 struct TurnosHospi_IOSApp: App {
-    // 2. Conectar el AppDelegate
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
-    // 3. Crear las instancias únicas de nuestros servicios
-    @StateObject var authService = AuthService()
-    @StateObject var shiftRepository = ShiftRepository()
-    
     var body: some Scene {
         WindowGroup {
-            // 4. Lógica de enrutamiento raíz
-            // Usamos RootView para decidir si mostrar Login o Main
-            RootView()
-                .environmentObject(authService)
-                .environmentObject(shiftRepository)
+            ContentView()
         }
     }
 }
