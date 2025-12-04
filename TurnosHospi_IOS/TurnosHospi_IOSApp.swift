ddb658ebc24d34414049f24849c63629f900feeb
Sote4 <andresmend12@gmail.com>
2025-12-04 10:38:43 +0100

    Initial commit

---
+++ TurnosHospi_IOS/TurnosHospi_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurnosHospi_IOSApp.swift
+//  TurnosHospi_IOS
+//
+//  Created by Andrés Mendoza Domínguez on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TurnosHospi_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
