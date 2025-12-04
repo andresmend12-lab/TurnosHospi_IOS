6f1577606220dced2d22c213af35906b0ebb734d
Sote4 <andresmend12@gmail.com>
2025-12-04 12:09:44 +0100

    vuelta

--- TurnosHospi_IOS/Models/PlantMembership.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct PlantMembership: Codable {
-    var plantId: String = ""
-    var userId: String = ""
-    var staffId: String? = nil
-    var staffName: String? = nil
-    var staffRole: String? = nil
-}
